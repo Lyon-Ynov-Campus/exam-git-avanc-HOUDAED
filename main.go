b0d7455763de95d4d318ac9d8043140a4490f5d2
hdagbegn <dagbegnonedvige.houmasse@ynov.com>
2025-10-27 16:11:04 +0100

    merging and cleaning

--- main.go
+++ main.go
@@ -13,13 +13,8 @@ func URLHandler(w http.ResponseWriter, r *http.Request) {
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "<h1>Site Web en Go</h1><p>Page d'accueil</p>")
-	})
-<<<<<<< HEAD
-=======
-	
+	})	
 	http.HandleFunc("/url", URLHandler)
->>>>>>> dev
-
 	fmt.Println("Serveur démarré sur http://localhost:8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
